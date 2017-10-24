7613fd441cc45f35e5ed471e9267946822289edf
Kyle Stark <kstark@prizelogic.net>
2017-10-24 16:13:01 -0700

    Build superstition page

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,24 @@ $('.imageblock .dots img').click(function() {
 
 });
 
+$('.superstition .imageblock .dots img').click(function() {
+    var dotindex = $(this).index();
+
+    $(this).attr('src','../assets/dotactive.png');
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active').attr('src','../assets/dot.png');
+    $(this).closest('.dots').siblings('.cardpic.active').removeClass('active');
+    $(this).closest('.dots').siblings('.cardpic').eq(dotindex).addClass('active');
+
+});
+
 var bclicks = 0;
 $('.buyingexpand').click(function() {
     $('.buyingmore').toggle();
-    
+
     if  (bclicks % 2 !== 0)  {
           $(this).html('Expand +');
-    } else { 
+    } else {
           $(this).html('Show less');
     }
     bclicks++;
@@ -37,10 +48,10 @@ return false;
 var sclicks = 0;
 $('.sellingexpand').click(function() {
     $('.sellingmore').toggle();
-    
+
     if  (sclicks % 2 !== 0)  {
           $(this).html('Expand +');
-    } else { 
+    } else {
           $(this).html('Show less');
     }
     sclicks++;
